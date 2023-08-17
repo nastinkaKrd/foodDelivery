27c0152508aa721e3bd2925a47e47fdd258b5871
nastinkaKrd <nastinka352@gmail.com>
2023-08-17 16:57:59 +0300

    cherry-pick files

--- target/generated-sources/annotations/com/project/food_delivery/mapper_interfaces/PlaceCategoryMapperImpl.java
+++ target/generated-sources/annotations/com/project/food_delivery/mapper_interfaces/PlaceCategoryMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-28T11:52:30+0300",
+    date = "2023-10-31T15:05:07+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 20 (Oracle Corporation)"
 )
 @Component
